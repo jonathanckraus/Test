ca0a3d9d67a56f2c80d0b47524edbc7f81d18dd8
jonathanckraus <163678222+jonathanckraus@users.noreply.github.com>
2024-03-23 20:09:48 -0400

    'b'

--- Add.py
+++ Add.py
@@ -1,7 +1,7 @@
 from time import gmtime, strftime
 a=6
 b=4
-print(a+b)
+print(f"The sum is  {a+b}")
 
-formatted_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+formatted_time = strftime("%m-%d-%Y %H:%M:%S")
 print("date and time:", formatted_time)
