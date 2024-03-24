321df5a1e19fbba6e22a5f4d0e426a094f990423
jonathanckraus <163678222+jonathanckraus@users.noreply.github.com>
2024-03-24 13:31:07 -0400

    add list

--- Add.py
+++ Add.py
@@ -1,7 +1,14 @@
 from time import gmtime, strftime
 a=6
 b=4
+sum=0
 print(f"The sum is  {a+b}")
+# Create a list of 20 items (for demonstration purposes)
+my_list = list(range(1, 21))
 
+# Enumerate through the list and print each item along with its index
+for index, item in enumerate(my_list):
+    sum += int(item)    
+    print(f"Item at index {index}: {item} sum {sum}")
 formatted_time = strftime("%m-%d-%Y %H:%M:%S")
 print("date and time:", formatted_time)
