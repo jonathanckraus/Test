eb82a6204a4e6eb79872a86a04824d651448b05e
jonathanckraus <163678222+jonathanckraus@users.noreply.github.com>
2024-03-24 23:36:49 -0400

    remov enum

--- Add.py
+++ Add.py
@@ -1,14 +1,17 @@
-from time import gmtime, strftime
+from time import strftime
 a=6
 b=4
 sum=0
-print(f"The sum is  {a+b}")
-# Create a list of 20 items (for demonstration purposes)
-my_list = list(range(1, 21))
+print(f"The sum is  {a+b}") 
 
+# Create a list of 20 items (for demonstration purposes)
+my_list = ("apple","banana","cherry")
+for item in(my_list):
+    print(item)
+print(my_list[1])
 # Enumerate through the list and print each item along with its index
-for index, item in enumerate(my_list):
-    sum += int(item)    
-    print(f"Item at index {index}: {item} sum {sum}")
+# for index, item in enumerate(my_list):
+#     sum += int(item)    
+#     print(f"Item at index {index}: {item} sum {sum}")
 formatted_time = strftime("%m-%d-%Y %H:%M:%S")
 print("date and time:", formatted_time)
